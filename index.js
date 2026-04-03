98999cdf8e29fe4a92987487b77210837b468bd8
JEROME-1122 <jeromfranklin123@gmail.com>
2026-04-03 22:28:19 +0530

    Add dotenv and dotenvx modules with comprehensive documentation
    - Introduced dotenv package (version 17.4.0) with configuration for loading environment variables from .env files.
    - Added detailed SKILL.md documentation for dotenv, covering usage, advanced features, and FAQs.
    - Introduced dotenvx for enhanced functionality including encryption, multiple environments, and variable expansion.
    - Added SKILL.md documentation for dotenvx, outlining installation, usage, encryption, and advanced features.

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 import app from "./app.js";
+import dotenv from "dotenv";
+dotenv.config();
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Welcome to the e-commerce API" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
