f6a9c7486fcb12e54e87cb3f6135aac5bf0dd1a4
JEROME-1122 <jeromfranklin123@gmail.com>
2026-04-14 16:43:49 +0530

    feat: add URL and URLSearchParams implementations with utility functions
    - Introduced urlencoded.js for parsing and serializing URL-encoded data.
    - Added utils.js for various object and property handling utilities.
    - Created package.json for managing dependencies and scripts for the whatwg-url package.
    - Implemented webidl2js-wrapper.js to export URL and URLSearchParams.

--- index.js
+++ index.js
@@ -5,12 +5,15 @@ import productRoutes from "./routes/productRoute.js";
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
+
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Welcome to the e-commerce API" });
 });
 
+
+// Routes
+app.use(express.json());
 app.use("/api", productRoutes);
 
 app.listen(PORT, () => {
