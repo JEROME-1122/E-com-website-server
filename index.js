187619fa4be79166f9d6a9714ef5a68946cf9d9c
JEROME-1122 <jeromfranklin123@gmail.com>
2026-04-14 16:31:24 +0530

    Reorder middleware setup by moving product routes below the root endpoint

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ import express from "express";
 const app = express();
 import dotenv from "dotenv";
 import productRoutes from "./routes/productRoute.js";
-
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use("/api", productRoutes);
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Welcome to the e-commerce API" });
 });
+
+app.use("/api", productRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
