93e842fb5289c2cce2c764bf2450f31142cc73ff
JEROME-1122 <jeromfranklin123@gmail.com>
2026-04-15 23:20:04 +0530

    feat: implement CRUD operations for products in the product controller and routes   without error handler

--- controller/productController.js
+++ controller/productController.js
@@ -1,14 +1,88 @@
-const getAllProducts = (req, res) => {
-  const products = [
-    { id: 1, name: "Product 1", price: 10.99 },
-    { id: 2, name: "Product 2", price: 19.99 },
-    { id: 3, name: "Product 3", price: 5.99 },
-  ];
-  res.status(200).json(products);
+//Create Product Controller
+import mongoose from "mongoose";
+import Product from "../models/productModel.js";
+
+export const addProduct = async (req, res) => {
+  //console.log(req.body);
+  const product = await Product.create(req.body);
+  res.status(200).json({
+    success: true,
+    product,
+  });
 };
-const getSingleProduct = (req, res) => {
-  const product = { id: 1, name: "Product 1", price: 10.99 };
-  res.status(200).json(product);
+
+//Get All Products
+const getAllProducts = async (req, res) => {
+  const products = await Product.find();
+  res.status(200).json({
+    success: true,
+    products,
+  });
+};
+
+//Get Single Product
+const getSingleProduct = async (req, res) => {
+  const id = req.params.id;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({
+      success: false,
+      message: "Invalid Product ID",
+    });
+  }
+  const product = await Product.findById(id);
+  if (!product) {
+    return res.status(404).json({
+      success: false,
+      message: "Product not found",
+    });
+  }
+  return res.status(200).json({
+    success: true,
+    product,
+  });
+};
+
+//Update product
+export const updateProduct = async (req, res) => {
+  const id = req.params.id;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({
+      success: false,
+      message: "Invalid Product ID",
+    });
+  }
+  const product = await Product.findByIdAndUpdate(id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  return res.status(200).json({
+    success: true,
+    product,
+  });
+};
+
+//Delete the product
+
+export const deleteProduct = async (req, res) => {
+  const id = req.params.id;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(500).json({
+      success: false,
+      message: "Invalid Product ID",
+    });
+  }
+  const product = await Product.findByIdAndDelete(id);
+  if (!product) {
+    return res.status(404).json({
+      success: false,
+      message: "Product not found",
+    });
+  }
+  return res.status(200).json({
+    success: true,
+    message: "Product deleted successfully",
+  });
 };
 
 export { getAllProducts, getSingleProduct };
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -3,11 +3,24 @@ import express from "express";
 import {
   getAllProducts,
   getSingleProduct,
+  addProduct,
+  updateProduct,
+  deleteProduct,
 } from "../controller/productController.js";
 
 const router = express.Router();
 
-router.get("/products", getAllProducts);
-router.get("/product", getSingleProduct);
+// router.get("/product/:id", getSingleProduct);
+// router.get("/products", getAllProducts);
+// router.post("/addProduct", addProduct);
+
+//function chaining (above 2 line we can be do same endpoind  get and post method)
+
+router.route("/products").get(getAllProducts).post(addProduct);
+router
+  .route("/product/:id")
+  .get(getSingleProduct)
+  .put(updateProduct)
+  .delete(deleteProduct);
 
 export default router;
